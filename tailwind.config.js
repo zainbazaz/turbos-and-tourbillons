cdda6a7b72620f31d1a73762f4ff6ccb44b9fca9
Zain Bazaz <zainbazaz96@gmail.com>
2022-09-26 21:30:20 +0530

    Styled index and login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./views/**/*.{ejs, html}", "./views/*.{ejs, html}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["night"],
+  },
 };
